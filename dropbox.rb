cad545bd2704cd2e0dccfedfa72f8fd0b9f0ab6d
Bèr Kessels <ber@dev.webschuur.com>
2010-05-26 22:45:17 +0200

    utodropImage class introduced * move image validation, creation and parsing into class * call metadata from this class in view.

--- dropbox.rb
+++ dropbox.rb
@@ -33,8 +33,9 @@ class DropboxController
       # then connect to dropbox
       # and fetch the metadata for the dir
       # For each file in that dir
-      Dir.glob("#{path}/*.{jpg,png,gif}", File::FNM_CASEFOLD).each do |img|
+      Dir.glob("#{path}/*.{jpg,png,gif}", File::FNM_CASEFOLD).each do |file|
         # is the file valid?
+        img = AutodropImage.new(file)
           # if so, then fetch the metadata for this file
           # and add this file and its metadata to the list of images.
           valid_images << img
@@ -80,23 +81,61 @@ class DropboxController
     return valid
   end
 
-  # see if an image exists, in the correct place, is an image, and not a special reserved thumb image.
-  def file_is_valid?(file)
-    # is it a valid pattern (i.e. does not contain funny characters)?
-    # all characters are valid, except for the /, directory limiter., files cannot start with a dot (hidden files)
-    # does the file exist?
-    # is the file an image?
-    # is the file not the special, reserved thumb.*? (see thumb_for_dir)
-
-    return true
-  end
-
   # determines the thumbnail for the dir: looks for tumb.*, if that is an image, else for the latest added image.
   def thumb_for_dir(dir)
     # if the dir contains an image thumb.* return that as thumb
     # else
       # loop trough all images in this dir
       # and return the last added one.
+  end
+end
+
+# Contains an autodrop image object
+# @TODO: fetch and parse exif and png comments and info
+class AutodropImage
+  attr_reader :filepath, :filename, :dirname
+
+  def initialize(filepath)
+    @filepath = filepath
+    @dirname = File.dirname(filepath)
+    @basename = File.basename(filepath)
+  end
+
+  # parse name, to human readable name
+  def title
+    #Flip off the part after the last dot, including that dot: find the filename without extensions
+    fragments = @basename.split('.')
+    fragments.pop
+    title = fragments.join('.')
+
+    return title.gsub(/[_+]/, ' ').capitalize
+  end
+
+  def src
+    "/#{@filepath}"
+  end
+
+  def permalink
+    path = without_container
+    p path
+    "/gallery/#{path}/#{@basename}"
+  end
+
+  # see if an image exists, in the correct place, is an image, and not a special reserved thumb image.
+  def valid?(file)
+    # is it a valid pattern (i.e. does not contain funny characters)?
+    # all characters are valid, except for the /, directory limiter., files cannot start with a dot (hidden files)
+    # does the file exist?
+    # is the file an image?
+    # is the file not the special, reserved thumb.*? (see thumb_for_dir)
+    return true
+  end
+
+  private
+  def without_container
+    fragments = @dirname.split('/')
+    p fragments.shift
+    title = fragments.join('/')
   end
 end
 
