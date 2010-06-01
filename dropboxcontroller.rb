e727e98259fa2d7efcd5eae53ee871d327983937
Bèr Kessels <ber@webschuur.com>
2010-06-01 16:58:17 +0200

    refactored code: little cleaner and slightly better OO.

--- dropboxcontroller.rb
+++ dropboxcontroller.rb
@@ -1,20 +1,26 @@
-class DropboxController
+class AutodropIndex
   # Pull sinatra config options into here.
-  def initialize(options)
+  def initialize(options, session=nil)
     @options = options
+
+    if session and session.authorized?
+      @session = session
+    else
+      @session = Dropbox::Session.deserialize(@options.session)
+    end
   end
 
   # returns a list of directories in the "containing dir" as set in config. possibly an empty list.
   # or returns nil if an error occurred (e.g. dir is not found)
-  def dirs
+  def galleries
     galleries = []
-    root_dir = AutodropGallery.new('/', @options)
+    root_dir = AutodropGallery.new('/', @options, @session)
 
     # For each directory in that dir
     root_dir.directory.ls.each do |d|
       # is this dir valid? then add to a list of dirs
       # else, continue, without adding to the list of dirs
-      gallery = AutodropGallery.new(d.path, @options) if d.directory?
+      gallery = AutodropGallery.new(d.path, @options, @session) if d.directory?
 
       galleries << gallery if gallery.valid?
     end
@@ -22,53 +28,32 @@ class DropboxController
     # return the list of dirs, even if it is an empty list.
     return galleries
   end
-
-  # returns a list of images in the given dir: possibly an empty list.
-  # or returns nil if an error occurred (e.g. dir is not found)
-  def images(gallery)
-    gallery = AutodropGallery.new(gallery, @options)
-
-    return nil if not gallery.valid?
-    # fetch the metadata for the dir
-    return gallery
-  end
-
-  def image(gallery, filename)
-    session = Dropbox::Session.deserialize(@options.session)
-    # do not continue if authorisation is false.
-    return nil unless session.authorized?
-
-    img = session.entry("/#{gallery}/#{filename}").metadata
-    img = AutodropImage.new(img, gallery, @options)
-
-    if img.valid?
-      return img
-    else
-      return nil
-    end
-  end
-
-  def mirror_file(path, file, size = 'm')
-    img = AutodropImage.new(file, path, @options)
-    img.mirror_file size
-  end
 end
 
 # Contains an autodrop image object
 # @TODO: fetch and parse exif and png comments and info
 class AutodropImage
-  attr_reader :file, :gallery
+  require 'ftools'
 
-  def initialize(file, gallery, options={})
-    @file = file
+  attr_reader :filename, :gallery, :path, :src
+  def initialize(filename, gallery, options={}, session=nil)
+    @filename = basename filename
     @gallery = gallery
     @options = options
+
+    @path = File.join(@gallery, @filename)
+    if session and session.authorized?
+      @session = session
+    else
+      @session = Dropbox::Session.deserialize(@options.session)
+    end
+    @entry = @session.entry(@path)
   end
 
   # parse name, to human readable name
   def title
     #Flip off the part after the last dot, including that dot: find the filename without extensions
-    fragments = basename.split('.')
+    fragments = @filename.split('.')
     fragments.pop
     title = fragments.join('.')
 
@@ -76,15 +61,11 @@ class AutodropImage
   end
 
   def src(size = 'm')
-    "#{@options.base_url}/image/#{size}#{@file.path}"
-  end
-
-  def path
-    @file.path
+    "#{@options.base_url}/image/#{size}/#{@path}"
   end
 
   def permalink
-    "/gallery/#{@gallery}/#{basename}"
+    "/gallery/#{@path}"
   end
 
   # see if an image exists, in the correct place, is an image, and not a special reserved thumb image.
@@ -93,21 +74,12 @@ class AutodropImage
     # all characters are valid, except for the /, directory limiter., files cannot start with a dot (hidden files)
     # is the file a web-savvy image?
     valid_types = ['image/jpeg', 'image/png', 'image/gif']
-    return false unless valid_types.include? @file.mime_type
+    return false unless valid_types.include? @entry.metadata.mime_type
     # does the file have thumbnails?
-    return false unless @file.thumb_exists
+    return false unless @entry.metadata.thumb_exists
     # is the file not the special, reserved thumb.*? (see thumb_for_dir)
     # @TODO: return false if basename.match /^thumb\.[A-z]*$/
     return true
-  end
-
-  def dirname
-    base = @file.path.split('/').shift
-    base.join('/')
-  end
-
-  def basename
-    @file.path.split('/').pop
   end
 
   # If local file do not exist, mirror it from dropbox, and return local filename
@@ -116,35 +88,41 @@ class AutodropImage
     # only allow whitelisted sizes.
     return nil unless ['m', 'l'].include? size
 
-    require 'ftools'
-
-    #TODO DRY
-    session = Dropbox::Session.deserialize(@options.session)
-    # do not continue if authorisation is false.
-    return nil unless session.authorized?
-
     basedir = "#{@options.cache_dir}/#{gallery}/#{size}"
     File.makedirs(basedir) unless File.exists?(basedir)
-    filepath = "#{basedir}/#{@file}"
-    drop_path = "#{@gallery}/#{@file}"
+    filepath = "#{basedir}/#{@filename}"
+    drop_path = "#{@gallery}/#{@filename}"
     if not File.exists? filepath
-      body = session.thumbnails(drop_path, size)
+      body = @session.thumbnail(drop_path, size)
       File.open(filepath, 'w') {|f| f.write(body) }
     end
 
     return filepath
   end
+
+  private
+  def basename(filename)
+    if filename.include?('/')
+      return filename.split('/').pop
+    else
+      return filename
+    end
+  end
+
 end
 
 class AutodropGallery
   attr_reader :gallery, :entry
-  def initialize(gallery, options)
+  def initialize(gallery, options, session=nil)
     @gallery = gallery
     @options = options
 
-    @session = Dropbox::Session.deserialize(@options.session)
-    # do not continue if authorisation is false.
-    # @TODO raise error, not return nil /# return nil unless session.authorized?
+    if session and session.authorized?
+      @session = session
+    else
+      @session = Dropbox::Session.deserialize(@options.session)
+    end
+    @entry
   end
 
   def directory
@@ -154,7 +132,7 @@ class AutodropGallery
   def images
     valid_images =[]
     @session.directory(@gallery).ls.each do |file|
-      img = AutodropImage.new(file, gallery, @options)
+      img = AutodropImage.new(file.path, gallery, @options, @session)
       # if so, then fetch the metadata for this file
       # and add this file and its metadata to the list of images.
       valid_images << img if img.valid?
@@ -169,7 +147,7 @@ class AutodropGallery
   end
 
   def thumb(size = 'm')
-    thumb = mirror_thumbnail
+    thumb = find_thumbnail
     return thumb
   end
 
@@ -185,7 +163,7 @@ class AutodropGallery
    return nil if entries.size <= 0
    #is at least one of those files a valid image?
    entries.each do |entry|
-     img = AutodropImage.new(entry, @gallery, @options)
+     img = AutodropImage.new(entry.path, @gallery, @options)
      return true if img.valid?
    end
 
@@ -193,7 +171,7 @@ class AutodropGallery
   end
 
   private
-  def mirror_thumbnail
+  def find_thumbnail
     imgs =[]
     # connect to remote, open dir, list entries
     entries = @session.directory(@gallery).ls.sort{|a, b| a.modified <=> b.modified }
@@ -201,7 +179,7 @@ class AutodropGallery
       if not entry.is_dir #is marked on dropbox as file
         if entry.thumb_exists #only TRUE when it is a file and omits many none-images
           # is that a valid image?
-          img = AutodropImage.new(entry, @gallery, @options)
+          img = AutodropImage.new(entry.path, @gallery, @options, @session)
           if img.valid?
             # is there a file with name thumb.*
             if /thumb\.[A-z]$/.match entry.path
